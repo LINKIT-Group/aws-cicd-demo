0a29e0f06ff0e5d258b549996b6c027dde3657be
Your Name <you@example.com>
2019-06-28 15:21:35 +0200

    fix var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 # service_name should be unique
 # used in naming cloud of services where possible
 variable "service_name" {
-  default = "app-${random_string.name.result}"
+  default = "app"
 }
 
 variable "repository_base_name" {
-  default = "app-${random_string.name.result}"
+  default = "app"
 }
 
 variable "region" {
